2277f903aeca2b0ce1f44206e7d0541c9fb3267b
pgalmor <47526823+PoleteTomaPedete@users.noreply.github.com>
2019-03-03 21:48:36 +0100

    Window 2.0

--- MyAwesomeGame/main.cpp
+++ MyAwesomeGame/main.cpp
@@ -3,10 +3,40 @@
 
 
 int main(int argc, char* argv[]) {
+	//Starting SDL
+	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+		return 1;
+	}
+	//Opening a Window
 	SDL_Window *win = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
 	if (win == nullptr) {
 		std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
 		SDL_Quit();
 		return 1;
 	}
+	//Creating a Renderer
+	SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (ren == nullptr) {
+		SDL_DestroyWindow(win);
+		std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return 1;
+	}
+	//Drawing the Texture
+	for (int i = 0; i < 3; ++i) {
+		//First clear the renderer
+		SDL_RenderClear(ren);
+		//Draw the texture
+		SDL_RenderCopy(ren, tex, NULL, NULL);
+		//Update the screen
+		SDL_RenderPresent(ren);
+		//Take a quick break after all that hard work
+		SDL_Delay(1000);
+	}
+	//Cleaning Up
+	SDL_DestroyTexture(tex);
+	SDL_DestroyRenderer(ren);
+	SDL_DestroyWindow(win);
+	SDL_Quit();
 }
