8ce1e6de23221099d56ff6711a64ee11d732658c
pgalmor <47526823+PoleteTomaPedete@users.noreply.github.com>
2019-03-07 20:00:31 +0100

    Bullet

--- main.cpp
+++ main.cpp
@@ -8,10 +8,27 @@
 
 using namespace std;
 
+#define Bullet_No 10 // Maximum no of alive bullet
+void shotRun();
+
 int main(int argc, char **argv) {
 	SDL_Window *mainFin;
 	SDL_Renderer *winRender;
 	SDL_Rect rectangle;
+
+	class _Bullet {
+	public:
+		bool alive; // For bullet alive checking
+		SDL_Rect shot; // For SDL fill rectangle
+		char w, h; // The width & height of a bullet
+	} bullet[Bullet_No];
+	// Initialize the bullet
+	for (int i = 0; i < Bullet_No; i++) {
+		bullet[i].alive = 0;
+		bullet[i].w = 75; // Bullet Size Configuration
+		bullet[i].h = 20;
+	}
+
 	bool winClose = true;
 	mainFin = SDL_CreateWindow("MyAwesomeGame by Pol Galan", 600, 100, 600, 600, SDL_WINDOW_RESIZABLE);
 	winRender = SDL_CreateRenderer(mainFin, -1, SDL_RENDERER_ACCELERATED);
@@ -41,12 +58,11 @@ int main(int argc, char **argv) {
 				case SDLK_UP:
 					rectangle.y = rectangle.y - 4;
 					break;
-
 				case SDLK_DOWN:
 					rectangle.y = rectangle.y + 4;
 					break;
 				case SDLK_SPACE:
-
+					
 					break;
 				}
 			}
@@ -61,6 +77,36 @@ int main(int argc, char **argv) {
 	SDL_DestroyWindow(mainFin);
 	SDL_Quit();
 	cin.get();
+
+
+	
+
+	while (shotRun) {
+
+		// Check if exist any empty slot for Initialize a bullet
+		if (bKeySpace == 1 && bKeySpaceTick % 10 == 1) {
+			for (int i = 0; i < Bullet_No; i++) if (!bullet[i].alive) {
+				bullet[i].alive = 1;
+				bullet[i].shot.x = rectangle.x + 200;
+				bullet[i].shot.y = rectangle.y + 40;
+				break; // break this for( ) loop
+			}
+		} // if(bKeyA == 1 && bKeyATick%10 == 1) { END
+		for (int i = 0; i < Bullet_No; i++) if (bullet[i].alive) {
+			bullet[i].shot.x += 5;
+			bullet[i].shot.w = bullet[i].w;
+			bullet[i].shot.h = bullet[i].h;
+			SDL_SetRenderDrawColor(winRender, 0, 255, 0, 0);
+			SDL_RenderFillRect(winRender, &bullet[i].shot);
+			// Bullet Destroy condition
+			if (bullet[i].shot.x > 600) bullet[i].alive = 0;
+		}
+
+		if (bKeySpace) bKeySpaceTick++; // Press KeyA counter + 1
+		if (!bKeySpace) bKeySpaceTick = 0; // Release KeyA counter = 0
+
+	}; // while(bRun) { END
+
 
 	return 0;
 }
